6c1824e0cf0e30e01dce0eac457de6998b55bb28
Sam Priestley <sfpriestley@hotmail.co.uk>
2020-11-15 18:21:56 +0000

    first test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,29 @@
 import pytest
+from brownie import Wei, config
+
+@pytest.fixture
+def live_dai_vault(Vault):
+    yield Vault.at('0x9B142C2CDAb89941E9dcd0B6C1cf6dEa378A8D7C')
+
+@pytest.fixture
+def live_dai_strategy(Strategy):
+    yield Strategy.at('0x4C6e9d7E5d69429100Fcc8afB25Ea980065e2773')
+
+@pytest.fixture
+def vault(gov, rewards, guardian, currency, pm):
+    Vault = pm(config["dependencies"][0]).Vault
+    vault = guardian.deploy(Vault, currency, gov, rewards, "", "")
+    yield vault
+
+@pytest.fixture
+def Vault(pm):
+    Vault = pm(config["dependencies"][0]).Vault
+    yield Vault
+
+
+@pytest.fixture(scope='session')
+def samdev(accounts):
+    yield accounts.at('0xC3D6880fD95E06C816cB030fAc45b3ffe3651Cb0', force=True)
 
 
 @pytest.fixture
@@ -15,8 +40,7 @@ def token(andre, Token):
 @pytest.fixture
 def gov(accounts):
     # yearn multis... I mean YFI governance. I swear!
-    yield accounts[1]
-
+    yield accounts.at('0x846e211e8ba920b353fb717631c015cf04061cc9', force=True)
 
 @pytest.fixture
 def rewards(gov):
@@ -54,63 +78,22 @@ def strategy(strategist, keeper, vault, Strategy):
     yield strategy
 
 
-@pytest.fixture
-def nocoiner(accounts):
-    # Has no tokens (DeFi is a ponzi scheme!)
-    yield accounts[5]
-
 
 @pytest.fixture
-def pleb(accounts, andre, token, vault):
-    # Small fish in a big pond
-    a = accounts[6]
-    # Has 0.01% of tokens (heard about this new DeFi thing!)
-    bal = token.totalSupply() // 10000
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
+def weth(interface):
+  
+    yield interface.ERC20('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
 
 @pytest.fixture
-def chad(accounts, andre, token, vault):
-    # Just here to have fun!
-    a = accounts[7]
-    # Has 0.1% of tokens (somehow makes money trying every new thing)
-    bal = token.totalSupply() // 1000
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
+def dai(interface):
+    yield interface.ERC20('0x6b175474e89094c44da98b954eedeac495271d0f')
 
 @pytest.fixture
-def greyhat(accounts, andre, token, vault):
-    # Chaotic evil, will eat you alive
-    a = accounts[8]
-    # Has 1% of tokens (earned them the *hard way*)
-    bal = token.totalSupply() // 100
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
-
-@pytest.fixture
-def whale(accounts, andre, token, vault):
-    # Totally in it for the tech
-    a = accounts[9]
-    # Has 10% of tokens (was in the ICO)
-    bal = token.totalSupply() // 10
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
+def whale(accounts, history, web3):
+    #big binance7 wallet
+    #acc = accounts.at('0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8', force=True)
+    #big binance8 wallet
+    #acc = accounts.at('0xf977814e90da44bfa03b6295a0616a897441acec', force=True)
+    #lots of weth account
+    acc = accounts.at('0x767Ecb395def19Ab8d1b2FCc89B3DDfBeD28fD6b', force=True)
+    yield acc
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,8 +1,61 @@
+from itertools import count
+from brownie import Wei, reverts
+from useful_methods import  genericStateOfVault,genericStateOfStrat
+import random
+import brownie
 # TODO: Add tests here that show the normal operation of this strategy
 #       Suggestions to include:
 #           - strategy loading and unloading (via Vault addStrategy/revokeStrategy)
 #           - change in loading (from low to high and high to low)
 #           - strategy operation at different loading levels (anticipated and "extreme")
 
-def test_operation():
-    pass
+
+
+def test_weth_del(web3, chain, Vault, Strategy, live_dai_vault, live_dai_strategy, whale, gov, dai,weth, samdev):
+    live_strategy = live_dai_strategy
+    live_vault = live_dai_vault
+
+    live_vault.setDepositLimit(1e28, {"from": gov})
+    live_vault.updateStrategyDebtLimit(live_strategy, 1e28, {"from": gov})
+
+    #increase balance of dai vault
+    
+
+    #deploy new strat
+    weth_vault = samdev.deploy(
+        Vault, weth, samdev, samdev, "", ""
+    )
+
+    weth.approve(weth_vault, 2 ** 256 - 1, {"from": whale} )
+
+    strategy = samdev.deploy(Strategy, weth_vault)
+
+    weth_vault.addStrategy(strategy, 2 ** 256 - 1, 2 ** 256 - 1, 50, {"from": samdev})
+
+    deposit_amount = Wei('100 ether')
+    weth_vault.deposit(deposit_amount, {"from": whale})
+
+
+    print("\n******* Harvest Weth ******")
+    strategy.harvest({'from': samdev})
+
+    print("\n******* Weth ******")
+    genericStateOfStrat(strategy, weth, weth_vault)
+    genericStateOfVault(weth_vault, weth)
+    print("\n******* Dai ******")
+    genericStateOfStrat(live_strategy, dai, live_vault)
+    genericStateOfVault(live_vault, dai)
+
+
+
+
+    print("\n******* Harvest Dai ******")
+    live_strategy.harvest({'from': samdev})
+
+    print("\n******* Weth ******")
+    genericStateOfStrat(strategy, weth, weth_vault)
+    genericStateOfVault(weth_vault, weth)
+    print("\n******* Dai ******")
+    genericStateOfStrat(live_strategy, dai, live_vault)
+    genericStateOfVault(live_vault, dai)
+
